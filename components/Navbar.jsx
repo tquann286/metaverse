2ec666d0eaaa0e1e2522a97fedae087c4abefc27
tquann286 <quantrung286@gmail.com>
2023-01-06 15:07:25 +0700

    init

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,26 @@
-'use client';
+'use client'
+
+import cn from 'classnames'
+import { motion } from 'framer-motion'
+import Image from 'next/image'
+
+import { APP_NAME } from '../constants'
+import styles from '../styles'
+import { navVariants } from '../utils/motion'
 
 const Navbar = () => (
-  <nav>
-    navbar
-  </nav>
-);
+  <motion.nav
+    variants={navVariants}
+    initial="hidden"
+    whileInView="show"
+    className={cn(styles.xPaddings, 'relative py-8')}
+  >
+    <div className="gradient-01 absolute inset-0 w-1/2" />
+    <div className={cn(styles.innerWidth, 'mx-auto flex justify-between gap-8')}>
+      <Image src="/search.svg" alt="search" width={24} height={24} className="object-contain" />
+      <h2 className="text-base font-extrabold leading-[30px] text-white">{APP_NAME}</h2>
+    </div>
+  </motion.nav>
+)
 
-export default Navbar;
+export default Navbar
--- constants/index.js
+++ constants/index.js
@@ -1,3 +1,5 @@
+export * from './Constants'
+
 export const exploreWorlds = [
   {
     id: 'world-1',
@@ -24,49 +26,48 @@ export const exploreWorlds = [
     imgUrl: '/planet-05.png',
     title: 'Hawkins Labs',
   },
-];
+]
 
 export const startingFeatures = [
   'Find a world that suits you and you want to enter',
   'Enter the world by reading basmalah to be safe',
   'No need to beat around the bush, just stay on the gas and have fun',
-];
+]
 
 export const newFeatures = [
   {
     imgUrl: '/vrpano.svg',
     title: 'A new world',
-    subtitle:
-        'we have the latest update with new world for you to try never mind',
+    subtitle: 'we have the latest update with new world for you to try never mind',
   },
   {
     imgUrl: '/headset.svg',
     title: 'More realistic',
     subtitle:
-        'In the latest update, your eyes are narrow, making the world more realistic than ever',
+      'In the latest update, your eyes are narrow, making the world more realistic than ever',
   },
-];
+]
 
 export const insights = [
   {
     imgUrl: '/planet-06.png',
     title: 'The launch of the Metaverse makes Elon musk ketar-ketir',
     subtitle:
-        'Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Diam maecenas sed enim ut sem viverra alique.',
+      'Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Diam maecenas sed enim ut sem viverra alique.',
   },
   {
     imgUrl: '/planet-07.png',
     title: '7 tips to easily master the madness of the Metaverse',
     subtitle:
-        'Vitae congue eu consequat ac felis donec. Et magnis dis parturient montes nascetur ridiculus mus. Convallis tellus id interdum',
+      'Vitae congue eu consequat ac felis donec. Et magnis dis parturient montes nascetur ridiculus mus. Convallis tellus id interdum',
   },
   {
     imgUrl: '/planet-08.png',
     title: 'With one platform you can explore the whole world virtually',
     subtitle:
-        'Quam quisque id diam vel quam elementum. Viverra nam libero justo laoreet sit amet cursus sit. Mauris in aliquam sem',
+      'Quam quisque id diam vel quam elementum. Viverra nam libero justo laoreet sit amet cursus sit. Mauris in aliquam sem',
   },
-];
+]
 
 export const socials = [
   {
@@ -85,4 +86,4 @@ export const socials = [
     name: 'facebook',
     url: '/facebook.svg',
   },
-];
+]
